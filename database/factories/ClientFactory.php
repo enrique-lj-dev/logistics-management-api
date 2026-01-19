9e61c9b83754b69849757eda99086e36489a09b9
Enrique <enriquelj.dev@gmail.com>
2026-01-19 03:20:58 +0100

    feat(shipments): create factory for client

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Client;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ClientFactory extends Factory
+{
+    protected $model = Client::class;
+
+    public function definition(): array
+    {
+        return [
+            'type' => 'b2c',
+            'name' => $this->faker->name,
+            'email' => $this->faker->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+            'address' => $this->faker->address,
+        ];
+    }
+}
