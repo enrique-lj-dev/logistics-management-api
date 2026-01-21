18ba2af258ae56bc4f53e7e6eea8c50d79ed075d
Enrique <enriquelj.dev@gmail.com>
2026-01-21 19:27:50 +0100

    refactor(api): simplify routes using apiResource

--- routes/api.php
+++ routes/api.php
@@ -5,18 +5,7 @@ use App\Http\Controllers\Api\ClientController;
 use App\Http\Controllers\Api\ShipmentController;
 
 Route::prefix('v1')->group(function () {
-
-    //Clients
-    Route::post('/clients', [ClientController::class, 'store']);
-    Route::get('/clients', [ClientController::class, 'index']);
-    Route::get('/clients/{client}', [ClientController::class, 'show']);
-    Route::put('/clients/{client}', [ClientController::class, 'update']);
-    Route::delete('/clients/{client}', [ClientController::class, 'destroy']);
-
-    //Shipments
-    Route::post('/shipments', [ShipmentController::class, 'store']);
-    Route::get('/shipments', [ShipmentController::class, 'index']);
-    Route::get('/shipments/{shipment}', [ShipmentController::class, 'show']);
-    Route::put('/shipments/{shipment}', [ShipmentController::class, 'update']);
-    Route::delete('/shipments/{shipment}', [ShipmentController::class, 'destroy']);
+    Route::apiResource('clients', ClientController::class);
+    Route::apiResource('shipments', ShipmentController::class);
 });
+
